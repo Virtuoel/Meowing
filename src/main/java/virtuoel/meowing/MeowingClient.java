548192fdb7e55879a42807138d480e54f8d47e83
Virtuoel <Virtuoel@users.noreply.github.com>
2022-08-18 02:19:41 +0300

    Un-italic custom stack name

--- src/main/java/virtuoel/meowing/MeowingClient.java
+++ src/main/java/virtuoel/meowing/MeowingClient.java
@@ -5,10 +5,12 @@ import org.lwjgl.glfw.GLFW;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.text.Text;
 
 public class MeowingClient implements ClientModInitializer
 {
@@ -24,6 +26,14 @@ public class MeowingClient implements ClientModInitializer
 			if (ACTION_KEY.wasPressed() && ClientPlayNetworking.canSend(Meowing.ACTION_PACKET))
 			{
 				ClientPlayNetworking.send(Meowing.ACTION_PACKET, new PacketByteBuf(Unpooled.buffer()));
+			}
+		});
+		
+		ItemTooltipCallback.EVENT.register((stack, context, lines) ->
+		{
+			if (!lines.isEmpty() && Meowing.isCat(stack))
+			{
+				lines.set(0, Text.empty().append(stack.getName()).formatted(stack.getRarity().formatting));
 			}
 		});
 	}
